b2858432c37f3f582d477fa2a8c159062c3f7856
Rok Andrée <rok@andree.si>
2016-04-26 17:57:31 +0200

    [] Simple enhancment for click logging

--- web/index.php
+++ web/index.php
@@ -20,7 +20,8 @@ $app->get('/{slug}', function (Silex\Application $app, Request $request, $slug) 
     }
 
     // Logs number of clicks
-    $redis->incr('num_'.$slug);
+    $redis->incr(sprintf('num_%s_%s', $slug, date('dmY')));
+    $redis->incr(sprintf('num_%s_all', $slug));
 
     $logLine = sprintf("%s::%s::%s::%s" . PHP_EOL, date('c'), $slug, $url, $_SERVER['REMOTE_ADDR']);
     file_put_contents(__DIR__ . '/../log/hits.log', $logLine, FILE_APPEND);
@@ -28,6 +29,5 @@ $app->get('/{slug}', function (Silex\Application $app, Request $request, $slug) 
     return $app->redirect($url);
 
 });
-
 
 $app->run();
