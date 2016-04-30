713ed619b4e7952d55065160ea2e1abe86120f7f
Rok Andrée <rok@andree.si>
2016-04-30 20:35:56 +0200

    [] Adds CORS support

--- web/index.php
+++ web/index.php
@@ -1,6 +1,13 @@
 <?php
 require_once __DIR__.'/../vendor/autoload.php';
 
+// I HATE CORS
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+	header('Access-Control-Allow-Methods: POST, PUT, DELETE, GET, HEAD');
+	header('Access-Control-Allow-Headers: X-Requested-With');
+  	exit;
+}
+
 use Symfony\Component\Yaml\Parser;
 
 $app = new \Silex\Application();
@@ -49,6 +56,5 @@ $app->get('/api/v1/domain', 'Controller\\Api::getAllEnabledDomains');
 
 $app->get('/error/{shortUrl}', 'Controller\\Error::missingShortUrl');
 $app->get('/error/invalid-domain', 'Controller\\Error::domainNotEnabled');
-
 
 $app->run();
